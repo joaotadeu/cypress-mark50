b0b076b7deabb14ed9b8cddc51e4ff023321ea7f
João Tadeu Soares Pereira <ojoaotadeu@gmail.com>
2024-05-14 15:40:40 -0300

    iniciando projeto MarkL gerenciador de tarefas

---
+++ cypress/e2e/tasks.cy.js
@@ -0,0 +1,35 @@
+// <reference types= cypress />
+
+describe('Validação da aplicação MarkL, criação de tarefas', () => {
+
+    it('deve cadastrar uma nova tarefa', () => {
+
+        const taskName = 'Ler um livro de Js'
+
+        cy.removeTaskByName(taskName)
+        cy.createTask(taskName)
+
+        cy.get('main div p')
+            .should('be.visible')
+            .should('have.text', taskName)
+
+        cy.contains('main div p', taskName)
+
+    })
+
+    it('não deve permitir tarefas duplicadas', () => {
+
+        const task = {
+            name: 'Ler um livro de Js',
+            is_done: false
+        }
+        
+        cy.removeTaskByName(task.name)
+        cy.postTask(task)
+        cy.createTask(task.name)
+
+        cy.get('.swal2-html-container')
+        .should('be.visible')
+        .should('have.text', 'Task already exists!')
+    })
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,29 @@
+Cypress.Commands.add('createTask', (taskName)=> {
+    cy.visit('http://localhost:3000/')
+    cy.get('input[placeholder="Add a new Task"]')
+        .type(taskName)
+    cy.contains('button', 'Create').click()
+    
+})
+
+Cypress.Commands.add('removeTaskByName', (taskName)=> {
+    cy.request({
+        url: 'http://localhost:3333/helper/tasks',
+        method: 'DELETE',
+        body: { name: taskName }
+    }).then(response => {
+        expect(response.status).to.eql(204)
+    })
+
+})
+
+Cypress.Commands.add('postTask', (task)=> {
+    cy.request({
+        url: 'http://localhost:3333/tasks',
+        method: 'POST',
+        body: task
+    }).then(response => {
+        expect(response.status).to.eql(201)
+    })
+
+})
